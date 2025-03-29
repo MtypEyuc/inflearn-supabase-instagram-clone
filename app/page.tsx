0a1d5acd9a4562313f6a39dcc7b98f524d5d2ff7
dud <Heo250113@gmail.com>
2025-03-30 08:46:49 +0900

    초기 버전

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export const metadata = {
+  title: "Inflearngram",
+  description: "Instagram clone project",
+};
+
+export default function Home() {
+  return (
+    <main>aadsad</main>
+  );
+}
