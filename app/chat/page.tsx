b4f1e862db54a24489f64bc4a7120717588ba886
dud <Heo250113@gmail.com>
2025-03-30 17:26:11 +0900

    feat: 접속 유저 리스트 구현 (2025-03-30) - 기간: 2025-03-30 - 내용: Instagram 클론코딩 - Supabase 인증 구현 (part 2) , Supabase Realtime 소개 + 채팅목록 구현

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,12 +1,17 @@
 import ChatPeopleList from "components/chat/ChatPeopleList";
 import ChatScreen from "components/chat/ChatScreen";
-import Person from "components/chat/Person";
+import { createServerSupabaseClient } from "utils/supabase/server";
 
-export default function ChatPage() {
-  return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <ChatPeopleList />
-      <ChatScreen />
-    </div>
-  );
+export default async function ChatPage() {
+    const supabase = await createServerSupabaseClient();
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    return (
+        <div className="w-full h-screen flex justify-center items-center">
+            <ChatPeopleList loggedInUser={session?.user} />
+            <ChatScreen />
+        </div>
+    );
 }
